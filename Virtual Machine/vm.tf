a35a11a1e3faa2a73fa205173a80725b08a73d78
Pankaj Tripathi <Pankajt2084@gmail.com>
2025-06-28 04:56:15 +0530

    created submodules for teeraform
    created submodules

---
+++ Virtual Machine/vm.tf
@@ -0,0 +1,60 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "vm-rg"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "demo-vnet"
+  address_space       = ["10.0.0.0/24"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "demo-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/28"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "demo-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "demo-vm"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+  size               = "Standard_B1s"
+  admin_username     = "azureuser"
+
+  admin_password     = "P@ssw0rd1234!"  # Not recommended. Use Key Vault or SSH keys.
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    name                 = "myosdisk1"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
---
+++ storage account/variables.tf
@@ -0,0 +1,10 @@
+variable "environment" {
+    type = string
+    description = "the env type"
+    default = "staging"
+}
+
+variable "subscription_id" {
+  type = string
+default     = "37fd4306-4815-4e3a-a2da-6905810ccba6"
+}
