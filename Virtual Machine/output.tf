265dfe942a0a582a1351a3c5264cfff913ed0146
Pankaj Tripathi <Pankajt2084@gmail.com>
2025-06-28 05:48:19 +0530

    updated backend

--- Virtual Machine/output.tf
+++ Virtual Machine/output.tf
@@ -1,3 +1,20 @@
-output "storage_account_name" {
-  value = azurerm_storage_account.example.name
+
+output "vm_name" {
+  value = azurerm_linux_virtual_machine.vm.name
+  description = "The name of the virtual machine"
+}
+
+output "vm_id" {
+  value = azurerm_linux_virtual_machine.vm.id
+  description = "The ID of the virtual machine"
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+  description = "Name of the resource group"
+}
+
+output "network_interface_id" {
+  value = azurerm_network_interface.nic.id
+  description = "The ID of the NIC attached to the VM"
 }
