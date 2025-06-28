cc50ff1a3aabfd796a35ba6fe8ed0c88cee2cd25
Pankaj Tripathi <Pankajt2084@gmail.com>
2025-06-28 07:27:21 +0530

    ip range fix

--- Virtual Machine/vm.tf
+++ Virtual Machine/vm.tf
@@ -1,15 +1,16 @@
+
 resource "azurerm_virtual_network" "vnet" {
-  name                = "app-vnet"
-  address_space       = ["10.0.0.0/24"]
+  name                = "minimal-vnet"
+  address_space       = ["10.0.0.0/24"]  # Small but enough to fit /29
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "app-subnet"
+  name                 = "minimal-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/28"]
+  address_prefixes     = ["10.0.0.0/29"]  # lower usable subnet
 }
 
 resource "azurerm_network_interface" "nic" {
